8418c3b1f7b73557bd6a7f2f2f99e510b1bd4343
Patrick Wilkes <pwilkes@thedutchdesigner.nl>
2018-11-14 21:14:41 +0100

    Eerste includes gemaakt en geplaatst

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php 
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "Patrick";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
+
+foreach ($db as $key => $value) {
+
+	define(strtoupper($key), $value);
+
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($connection) {
+	echo "Hij doet het!!";
+} else {
+	echo "Hij werkt nog niet!!!";
+}
+
+ ?>
---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+
+</html>
